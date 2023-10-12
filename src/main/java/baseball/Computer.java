82c32f2cc16d94db45a3af652e183b87f139e469
minseo  jo <liging12@naver.com>
2023-10-13 00:55:24 +0900

    feat: 컴퓨터가 서로 다른 임의의 수 3개를 선택하는 기능 추가

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Computer {
+    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int NUMBER_OF_DIGITS = 3;
+    private List<Integer> uniqueNumbers;
+
+    Computer(){
+        generateUniqueNumbers();
+    }
+
+    public List<Integer> getUniqueNumbers() {
+        return this.uniqueNumbers;
+    }
+
+    private void generateUniqueNumbers() {
+        while (uniqueNumbers.size() < NUMBER_OF_DIGITS) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+            if (!uniqueNumbers.contains(randomNumber)) {
+                uniqueNumbers.add(randomNumber);
+            }
+        }
+    }
+
+}
