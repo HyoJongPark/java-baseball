5753a9a68f82a7b3aec4b8e8779947028cd5c556
minseo  jo <liging12@naver.com>
2023-10-13 23:34:27 +0900

    refactor: 에러를 반환시 바로 프로그램 종료 하므로, throws 를 하지 않고 바로 에러 반환

--- src/main/java/baseball/view/input/InputValidator.java
+++ src/main/java/baseball/view/input/InputValidator.java
@@ -3,5 +3,5 @@ package baseball.view.input;
 import java.util.List;
 
 public interface InputValidator {
-    boolean isValidInput(List<Integer> playerInput);
+    boolean isValidInput(List<Integer> playerInput) throws IllegalArgumentException;
 }
