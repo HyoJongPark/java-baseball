77d34188d6b9200f58d98f34f131ccbf291c6ea1
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-17 15:06:06 +0900

    feat: 랜덤 숫자 3개 뽑기 기능 구현

---
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class NumberGenerator {
+
+    private List<Integer> generatedNumbers = new ArrayList<>();
+
+    public NumberGenerator() {
+        List<Integer> numbers = new ArrayList<>();
+        IntStream.rangeClosed(1, 9).forEach(numbers::add);
+        Collections.shuffle(numbers);
+
+        IntStream.range(0, 3).forEach(i -> generatedNumbers.add(numbers.get(i)));
+    }
+}
